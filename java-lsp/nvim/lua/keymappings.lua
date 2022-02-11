6b9fa82454d2782a0cb927ae9b31bc2cbf3cf8c3
Richard Agyei <richard.agyei@gmail.com>
2022-02-10 21:26:56 -0600

    Ansible setup

---
+++ java-lsp/nvim/lua/keymappings.lua
@@ -0,0 +1,30 @@
+local utils = require("settings/utils")
+-- explorer
+utils.map('n', '<Leader>e', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
+utils.map('n', '-', ':RnvimrToggle<CR>', {noremap = true, silent = true})
+
+-- better window movement
+utils.map('n', '<C-h>', '<C-w>h', {silent = true})
+utils.map('n', '<C-j>', '<C-w>j', {silent = true})
+utils.map('n', '<C-k>', '<C-w>k', {silent = true})
+utils.map('n', '<C-l>', '<C-w>l', {silent = true})
+
+-- escape from insert mode
+utils.map("i", "jk", "<ESC>")
+utils.map("i", "kj", "<ESC>")
+utils.map("i", "jj", "<ESC>")
+
+utils.map("n", "<leader>p", '"_dp')
+
+-- quick splits
+utils.map("n", "<leader>v", ':vsplit<CR>')
+utils.map("n", "<leader>h", ':split<CR>')
+
+-- Y yank until the end of line
+utils.map("n", "Y", "y$")
+-- map('n', '<leader>/', '<cmd>noh<CR>') -- Clear highlights
+utils.map("n", "<A-/>", "<cmd>noh<CR>") -- Clear highlights
+utils.map("i", "jk", "<Esc>")
+utils.map("", "<leader>c", '"+y') --Copy to clipboard in normal, visual, select and operator modes
+utils.map("i", "<C-u>", "<C-g>u<C-u>") -- Make <C-u> undo-friendly
+utils.map("i", "<C-w>", "<C-g>u<C-w>") -- Make <C-w> undo-friendly
---
+++ java-lsp/nvim/lua/settings/general.lua
@@ -0,0 +1,4 @@
+local opt = vim.opt  -- to set options
+-- "unnamed,unnamedplus"
+-- vim.o.clipboard += "unnamedplus"
+opt.clipboard = "unnamed,unnamedplus"
