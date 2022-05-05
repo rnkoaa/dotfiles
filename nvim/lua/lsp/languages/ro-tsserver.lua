63c29a26e91f9102d03dc23f409ecf3a40888103
Richard Agyei <richard.agyei@gmail.com>
2022-05-05 05:11:53 -0500

    tgt updates

--- nvim/lua/lsp/languages/ro-tsserver.lua
+++ nvim/lua/lsp/languages/ro-tsserver.lua
@@ -1,5 +1,4 @@
-
 -- Setup lspconfig.
-require("lspconfig").tsserver.setup {
-  capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
-}
+require("lspconfig").tsserver.setup({
+	capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+})
