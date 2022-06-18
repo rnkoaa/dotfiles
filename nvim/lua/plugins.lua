a137ae16e15240b26ab6c9bb9eb95d22845f0d66
Richard Agyei <richard.agyei@gmail.com>
2022-06-18 11:01:02 -0500

    Cleanup

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -71,9 +71,6 @@ require("packer").startup(function()
 
 	use({
 		"jose-elias-alvarez/null-ls.nvim",
-		-- config = function()
-		-- 	require("null-ls").setup()
-		-- end,
 		requires = { "nvim-lua/plenary.nvim" },
 	})
 end)
