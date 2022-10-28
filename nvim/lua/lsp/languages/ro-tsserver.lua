27d4c246a2497def59e8e283147bfbe978e557a0
Richard Agyei <richard.agyei@gmail.com>
2022-10-28 05:09:57 -0500

    typescript notes

--- nvim/lua/lsp/languages/ro-tsserver.lua
+++ nvim/lua/lsp/languages/ro-tsserver.lua
@@ -1,3 +1,8 @@
+-- https://medium.com/prodhacker/my-neovim-setup-for-react-typescript-tailwind-css-etc-in-2022-a7405862c9a4
+-- https://blog.inkdrop.app/how-to-set-up-neovim-0-5-modern-plugins-lsp-treesitter-etc-542c3d9c9887
+-- https://github.com/jose-elias-alvarez/typescript.nvim
+-- https://github.com/albingroen/nvim-lsp-typescript-config
+-- https://blog.ffff.lt/posts/typescript-and-neovim-lsp/
 -- Setup lspconfig.
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
