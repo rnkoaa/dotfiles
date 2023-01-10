06801dd2e512cb0bd05329e8e6182f31116f55bd
Richard Agyei <richard.agyei@gmail.com>
2023-01-10 03:07:33 -0600

    README

--- nvim/lua/rnkoaa/plugins/nvim-tree.lua
+++ nvim/lua/rnkoaa/plugins/nvim-tree.lua
@@ -1,7 +1,7 @@
 -- init.lua
 local setup, nvimtree = pcall(require, "nvim-tree")
 if not setup then
-  return
+	return
 end
 
 -- recommended settings from nvim-tree documentation
@@ -11,13 +11,15 @@ vim.g.loaded_netrwPlugin = 1
 -- setup with all defaults
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
 nvimtree.setup({
-
-  actions = {
-    open_file = {
-      window_picker = {
-        enable = false,
-      },
-    },
-  },
-
+	-- update_focused_file = {
+	-- 	enabled = true,
+	-- },
+	--
+	actions = {
+		open_file = {
+			window_picker = {
+				enable = false,
+			},
+		},
+	},
 }) -- END_DEFAULT_OPTS
