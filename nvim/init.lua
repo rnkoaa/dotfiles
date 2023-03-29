5fc55702c0d77baccbfeab8a06058c7ccdd4c335
Richard Agyei <richard.agyei@gmail.com>
2023-03-28 21:32:57 -0500

    Lazy kickstart

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,74 +1,46 @@
-local utils = require("settings.utils")
-
--- Install packer
-local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-	vim.fn.execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-vim.api.nvim_exec(
-	[[
-  augroup Packer
-    autocmd!
-    autocmd BufWritePost init.lua PackerCompile
-  augroup end
-]],
-	false
-)
-
-require("plugins")
-require("mind").setup()
-
-require("nvim-autopairs").setup()
--- require("lspkind").init()
-
--- setup must be called before loading
-vim.cmd("colorscheme nordfox")
-
-require("lualine").setup({
-	theme = "nordfox",
-})
-
---Remap space as leader key
-vim.api.nvim_set_keymap("", "<Space>", "<Nop>", { noremap = true, silent = true })
-vim.g.mapleader = " "
+-- Example using a list of specs with the default options
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 vim.g.maplocalleader = " "
 
---Remap for dealing with word wrap
-local opts = { noremap = true, expr = true, silent = true }
-vim.api.nvim_set_keymap("n", "k", "v:count == 0 ? 'gk' : 'k'", opts)
-vim.api.nvim_set_keymap("n", "j", "v:count == 0 ? 'gj' : 'j'", opts)
+require("plugins")
+require("rnkoaa.core.keymaps")
+require("rnkoaa.plugins.comment")
+-- require("rnkoaa.core.colors")
+require("rnkoaa.core.settings")
 
--- Y yank until the end of line  (note: this is now a default on master)
-vim.api.nvim_set_keymap("n", "Y", "y$", { noremap = true })
+require("rnkoaa.plugins.lualine")
+require("rnkoaa.plugins.gitsigns")
+require("rnkoaa.plugins.indent_blankline")
+require("rnkoaa.plugins.telescope")
+require("rnkoaa.plugins.treesitter")
+require("rnkoaa.plugins.neodev")
+require("rnkoaa.plugins.cmp")
+require("rnkoaa.plugins.lspsaga")
+require("rnkoaa.plugins.autopairs")
+-- require("rnkoaa.plugins.nvim-tree")
+require("rnkoaa.plugins.toggleterm")
+require("rnkoaa.plugins.trouble")
+require("rnkoaa.plugins.which_key")
 
---Map blankline
-vim.g.indent_blankline_char = "┊"
-vim.g.indent_blankline_filetype_exclude = { "help", "packer" }
-vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
-vim.g.indent_blankline_char_highlight = "LineNr"
-vim.g.indent_blankline_show_trailing_blankline_indent = false
+-- lsp, linters and formatters
+require("rnkoaa.lsp")
+-- require("rnkoaa.lsp.mason")
+require("rnkoaa.lsp.lspconfig")
+require("rnkoaa.plugins.null-ls")
 
--- Telescope
-require("lsp")
-require("lsp.config.ro-nvimcmp")
-require("lspconfig").sqls.setup({})
+vim.cmd[[colorscheme tokyonight]]
 
-require("settings.general")
-require("lsp.config.ro-telescope")
-require("lsp.languages.ro-tsserver")
-require("lsp.languages.ro-kotlin")
-require("lsp.go-ls")
--- require("lsp.config.ro-keybindings")
--- require("lsp.config.ro-formatter")
--- require("lsp.config.ro-formatter")
-require("lsp.config.ro-whichkey")
-require("rnkoaa.toggleterm")
-require("rnkoaa.nvim-tree")
-require("rnkoaa.keymappings")
-require("rnkoaa.trouble")
-require("rnkoaa.null-ls").setup()
-require("nvim-ts-autotag").setup()
-require("gitsigns").setup()
-require("config.snip").setup()
+require("tokyonight").setup({})
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,119 +1,91 @@
-local use = require("packer").use
-require("packer").startup(function()
-	-- For Packer
-	use("wbthomason/packer.nvim")
-
+require("lazy").setup({
 	-- Explorer
-	use({
-		"kyazdani42/nvim-tree.lua",
-		requires = { "kyazdani42/nvim-web-devicons" },
-	})
-
-	use({
+	{
+  "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    dependencies = { 
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    }
+  },
 		"iamcco/markdown-preview.nvim",
-	})
-
-	use({
 		"folke/which-key.nvim",
-	})
 
-	use("neovim/nvim-lspconfig")
-	use("onsails/lspkind-nvim")
-
-	use({
+	"neovim/nvim-lspconfig",
+	"onsails/lspkind-nvim",
+	{
 		"glepnir/lspsaga.nvim",
 		branch = "main",
-	})
-
-	use({ "TimUntersberger/neogit", requires = "nvim-lua/plenary.nvim" })
-	use({
+	},
+	{ "TimUntersberger/neogit", dependencies = "nvim-lua/plenary.nvim" },
+	{
 		"lewis6991/gitsigns.nvim",
 		tag = "release", -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
-	})
-
-	use({
+	},
+	{
 		"sindrets/diffview.nvim",
-		requires = { "nvim-lua/plenary.nvim" },
-	})
-
-	use({
-		"davidgranstrom/nvim-markdown-preview",
-	})
-
-	use({
-		"phaazon/mind.nvim",
-		branch = "v2.2",
-		requires = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("mind").setup()
-		end,
-	})
-
-	use("hrsh7th/nvim-cmp")
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-cmdline")
-
-	use("L3MON4D3/LuaSnip") -- Snippets plugin
-  use("rafamadriz/friendly-snippets")
-  use("honza/vim-snippets")
-	use("saadparwaiz1/cmp_luasnip")
-
-	use("EdenEast/nightfox.nvim")
-	use("windwp/nvim-autopairs")
-	use("windwp/nvim-ts-autotag")
-	use("maxmellon/vim-jsx-pretty")
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	"hrsh7th/nvim-cmp",
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
+	"hrsh7th/cmp-cmdline",
+	"L3MON4D3/LuaSnip", -- Snippets plugin
+  "rafamadriz/friendly-snippets",
+  "honza/vim-snippets",
+	"saadparwaiz1/cmp_luasnip",
+	'folke/tokyonight.nvim',
+	"windwp/nvim-autopairs",
+	"windwp/nvim-ts-autotag",
+	"maxmellon/vim-jsx-pretty",
 	-- Highlight, edit, and navigate code using a fast incremental parsing library
-	use("nvim-treesitter/nvim-treesitter")
-	use("nvim-lualine/lualine.nvim")
+	"nvim-treesitter/nvim-treesitter",
+	"nvim-lualine/lualine.nvim",
 	-- Additional textobjects for treesitter
-	use("nvim-treesitter/nvim-treesitter-textobjects")
+	"nvim-treesitter/nvim-treesitter-textobjects",
 
-	use("tpope/vim-commentary") -- "gc" to comment visual regions/lines
-	use("tpope/vim-surround") -- "ys\"" to surround a text with "", "cs'\"" to change surrounding quotes
-	use("tpope/vim-repeat") -- "repeat commands"
+	"tpope/vim-commentary", -- "gc" to comment visual regions/line,
+	"tpope/vim-surround", -- "ys\"" to surround a text with "", "cs'\"" to change surrounding quotes
+	"tpope/vim-repeat", -- "repeat commands"
+	"mattn/emmet-vim",
+	
 	-- Add indentation guides even on blank lines
-	use("lukas-reineke/indent-blankline.nvim")
-	use({
+	"lukas-reineke/indent-blankline.nvim",
+	{
 		"nvim-telescope/telescope.nvim",
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
-	use({ "nvim-telescope/telescope-ui-select.nvim" })
+		dependencies = { { "nvim-lua/plenary.nvim" } },
+	},
+	{ "nvim-telescope/telescope-ui-select.nvim" },
 
-	use("mfussenegger/nvim-jdtls")
-
-	use("nanotee/sqls.nvim")
+	"mfussenegger/nvim-jdtls",
+	"nanotee/sqls.nvim",
 
 	-- distraction free writing
-	use({ "junegunn/goyo.vim" })
-	use({
+	{ "junegunn/goyo.vim" },
+	{
 		"akinsho/toggleterm.nvim",
-		tag = "v2.*",
+		tag = "2.4.0",
 		config = function()
 			require("toggleterm").setup()
 		end,
-	})
-
-	use({
+	},
+	{
 		"jose-elias-alvarez/null-ls.nvim",
-		requires = { "nvim-lua/plenary.nvim" },
-	})
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
 
 	-- method signature support
-	use({
+	{
 		"ray-x/lsp_signature.nvim",
-	})
-
-	use("mattn/emmet-vim")
-
-	use({
+	},
+	{
 		"folke/trouble.nvim",
-		requires = "kyazdani42/nvim-web-devicons",
-	})
-
-	use({
+		dependencies = "nvim-tree/nvim-web-devicons",
+	},
+	{
 		"dsznajder/vscode-es7-javascript-react-snippets",
-		run = "yarn install --frozen-lockfile && yarn compile",
-	})
-end)
+		build = "yarn install --frozen-lockfile && yarn compile",
+	}
+})
