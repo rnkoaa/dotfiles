d4248153a2d28913b8b26b7976e2deb524235b43
Richard Agyei <richard.agyei@gmail.com>
2023-04-25 21:11:26 -0500

    no mason

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -1,90 +1,94 @@
 --LSP Configuration & Plugins
 local M = {
-  {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      -- Additional lua configuration, makes nvim stuff amazing
-      { "folke/neoconf.nvim", cmd = "Neoconf", config = true },
-      { "folke/neodev.nvim", config = true },
-      -- Useful status updates for LSP
-      { "j-hui/fidget.nvim", config = true },
-      "onsails/lspkind-nvim",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-nvim-lsp-signature-help",
-    },
-    opts = {
-      servers = {
-        cssls = {},
-        docker_ls = {},
-        emmet_ls = {
-          filetypes = {
-            "html",
-            "typescriptreact",
-            "javascriptreact",
-            "css",
-            "sass",
-            "scss",
-            "less",
-            "svelte",
-          },
-        },
-        html = {},
-        jsonls = {
-          settings = {
-            json = {},
-          },
-        },
-        lua_ls = {
-          settings = {
-            Lua = {
-              workspace = {
-                checkThirdParty = false,
-              },
-              completion = { callSnippet = "Replace" },
-              telemetry = { enable = false },
-              hint = {
-                enable = false,
-              },
-            },
-          },
-        },
-        yamlls = {},
-      },
-      setup = {
-        lua_ls = function(_, _)
-          local lsp_utils = require("plugins.lsp.utils")
-          lsp_utils.on_attach(function(client, buffer)
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			-- Additional lua configuration, makes nvim stuff amazing
+			{ "folke/neoconf.nvim", cmd = "Neoconf", config = true },
+			{ "folke/neodev.nvim", config = true },
+			-- Useful status updates for LSP
+			{ "j-hui/fidget.nvim", config = true },
+			"onsails/lspkind-nvim",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-nvim-lsp-signature-help",
+		},
+		opts = {
+			servers = {
+				cssls = {},
+				docker_ls = {},
+				emmet_ls = {
+					filetypes = {
+						"html",
+						"typescriptreact",
+						"javascriptreact",
+						"css",
+						"sass",
+						"scss",
+						"less",
+						"svelte",
+					},
+				},
+				html = {},
+				jsonls = {
+					settings = {
+						json = {},
+					},
+				},
+				lua_ls = {
+					settings = {
+						Lua = {
+							runtime = {
+								-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+								version = "LuaJIT",
+							},
+							workspace = {
+								checkThirdParty = false,
+							},
+							completion = { callSnippet = "Replace" },
+							telemetry = { enable = false },
+							hint = {
+								enable = false,
+							},
+						},
+					},
+				},
+				yamlls = {},
+			},
+			setup = {
+				lua_ls = function(_, _)
+					local lsp_utils = require("plugins.lsp.utils")
+					lsp_utils.on_attach(function(client, buffer)
 					       -- stylua: ignore
 					       if client.name == "lua_ls" then
 					         vim.keymap.set("n", "<leader>dX", function() require("osv").run_this() end, { buffer = buffer, desc = "OSV Run" })
 					         vim.keymap.set("n", "<leader>dL", function() require("osv").launch({ port = 8086 }) end, { buffer = buffer, desc = "OSV Launch" })
 					       end
-          end)
-        end,
-      },
-    },
-    config = function(plugin, opts)
-      require("plugins.lsp.servers").setup(plugin, opts)
-    end,
-  },
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    opts = function()
-      local nls = require("null-ls")
-      return {
-        root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
-        sources = {
-          nls.builtins.formatting.fish_indent,
-          nls.builtins.diagnostics.fish,
-          nls.builtins.formatting.stylua,
-          nls.builtins.formatting.shfmt,
-          -- nls.builtins.diagnostics.flake8,
-        },
-      }
-    end,
-  },
+					end)
+				end,
+			},
+		},
+		config = function(plugin, opts)
+			require("plugins.lsp.servers").setup(plugin, opts)
+		end,
+	},
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = function()
+			local nls = require("null-ls")
+			return {
+				root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+				sources = {
+					nls.builtins.formatting.fish_indent,
+					nls.builtins.diagnostics.fish,
+					nls.builtins.formatting.stylua,
+					nls.builtins.formatting.shfmt,
+					-- nls.builtins.diagnostics.flake8,
+				},
+			}
+		end,
+	},
 }
 
 return M
