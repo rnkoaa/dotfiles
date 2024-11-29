f89f015d1947a33c21dd10e587a467196b51a386
Richard Agyei <richard.agyei@gmail.com>
2024-11-29 10:01:36 -0600

    cleanup

---
+++ nvim/old-lua/config/autocmds.lua
@@ -0,0 +1,133 @@
+-- Highlight on yank
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = "*",
+})
+
+
+-- Check if we need to reload the file when it changed
+vim.api.nvim_create_autocmd("FocusGained", { command = "checktime" })
+
+vim.api.nvim_create_autocmd("BufReadPre", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "<buffer>",
+      once = true,
+      callback = function()
+        vim.cmd(
+          [[if &ft !~# 'commit\|rebase' && line("'\"") > 1 && line("'\"") <= line("$") | exe 'normal! g`"' | endif]]
+        )
+      end,
+    })
+  end,
+})
+
+-- windows to close
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {
+    "OverseerForm",
+    "OverseerList",
+    "floggraph",
+    "fugitive",
+    "git",
+    "help",
+    "lspinfo",
+    "man",
+    "neotest-output",
+    "neotest-summary",
+    "qf",
+    "query",
+    "spectre_panel",
+    "startuptime",
+    "toggleterm",
+    "tsplayground",
+    "vim",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
+
+-- show cursor line only in active window
+vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
+  callback = function()
+    local ok, cl = pcall(vim.api.nvim_win_get_var, 0, "auto-cursorline")
+    if ok and cl then
+      vim.wo.cursorline = true
+      vim.api.nvim_win_del_var(0, "auto-cursorline")
+    end
+  end,
+})
+vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
+  callback = function()
+    local cl = vim.wo.cursorline
+    if cl then
+      vim.api.nvim_win_set_var(0, "auto-cursorline", cl)
+      vim.wo.cursorline = false
+    end
+  end,
+})
+
+--[[
+
+-- go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = augroup("last_loc"),
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("close_with_q"),
+  pattern = {
+    "PlenaryTestPopup",
+    "help",
+    "lspinfo",
+    "man",
+    "notify",
+    "qf",
+    "spectre_panel",
+    "startuptime",
+    "tsplayground",
+    "checkhealth",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
+
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("wrap_spell"),
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
+})
+
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = augroup("auto_create_dir"),
+  callback = function(event)
+    if event.match:match("^%w%w+://") then
+      return
+    end
+    local file = vim.loop.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
+--]]
---
+++ nvim/old-lua/plugins/lsp/init.lua
@@ -0,0 +1,92 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      { "folke/neoconf.nvim",      cmd = "Neoconf", config = true },
+      { "smjonas/inc-rename.nvim", config = true },
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    opts = {
+      servers = {
+        bashls = {},
+        cssls = {},
+        dockerls = {},
+        docker_compose_language_service = {},
+        emmet_ls = {},
+        gopls = {},
+        gradle_ls = {},
+        groovyls = {},
+        html = {},
+        jsonls = {},
+        lua_ls = {
+          settings = {
+            Lua = {
+              workspace = {
+                checkThirdParty = false,
+              },
+              completion = { callSnippet = "Replace" },
+              telemetry = { enable = false },
+              hint = {
+                enable = false,
+              },
+            },
+          },
+        },
+        sqlls = {},
+        ts_ls = {},
+        vimls = {},
+        yamlls = {},
+      },
+      setup = {
+        lua_ls = function(_, _)
+          local lsp_utils = require("plugins.lsp.utils")
+          lsp_utils.on_attach(function(client, buffer)
+            stylua: ignore
+            if client.name == "lua_ls" then
+              vim.keymap.set("n", "<leader>dX", function() require("osv").run_this() end,
+                { buffer = buffer, desc = "OSV Run" })
+              vim.keymap.set("n", "<leader>dL", function() require("osv").launch({ port = 8086 }) end,
+                { buffer = buffer, desc = "OSV Launch" })
+            end
+          end)
+        end,
+      },
+    },
+    config = function(plugin, opts)
+      require("plugins.lsp.servers").setup(plugin, opts)
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = "BufReadPre",
+    config = function()
+      local nls = require("null-ls")
+      nls.setup({
+        sources = {
+          nls.builtins.formatting.stylua,
+          -- nls.builtins.formatting.black,
+          nls.builtins.formatting.shfmt,
+          nls.builtins.diagnostics.ruff.with({ extra_args = { "--max-line-length=180" } }),
+        },
+      })
+    end,
+  },
+  {
+    "folke/trouble.nvim",
+    cmd = { "TroubleToggle", "Trouble" },
+    opts = { use_diagnostic_signs = true },
+    keys = {
+      { "<leader>cd", "<cmd>TroubleToggle document_diagnostics<cr>",  desc = "Document Diagnostics" },
+      { "<leader>cD", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics" },
+    },
+  },
+  {
+    "glepnir/lspsaga.nvim",
+    event = "VeryLazy",
+    config = true,
+  },
+  -- { "rafcamlet/nvim-luapad", cmd = { "LuaRun", "Luapad" } },
+}
